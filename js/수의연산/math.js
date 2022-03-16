6c105fd817a634a7cea5079552f19617f587822b
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-03-16 10:52:27 +0900

    update

---
+++ js/수의연산/math.js
@@ -0,0 +1,22 @@
+/* 
+    [수의 연산 - Math 객체의 메소드]
+
+    1. Math.pow(인자1, 인자2)
+    
+    = 인자1의 인자2만큼의 제곱을 출력하는 함수
+
+    2. Math.round()
+
+    = 소수점 이하 반올림하는 함수
+*/
+
+
+Math.pow(3, 2);
+
+// console.log(Math.pow(3, 2));
+// 결과값 9
+
+Math.round(10.66);
+
+// console.log(Math.round(10.66));
+// 결과값 11
