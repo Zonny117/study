636b9648b58aec61ed0515b5747aa3fcd2a9a23b
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-03-16 16:38:30 +0900

    a

---
+++ js/문자연산/string.js
@@ -0,0 +1,31 @@
+/* 
+    [역슬래시 \]
+    문자열에서 역슬래시 바로 뒤따르는 1개의 텍스트를 문자로 해석한다. = escape
+*/
+
+//  예
+
+// console.log('zonny\'s test area');
+
+// 결과값 "zonny's test area"
+
+/* 
+    역슬래시 바로 뒤 작은 따옴표가 문자열의 끝을 의미하지 않고, 하나의 문자로 인식된다.
+*/
+
+/* 
+    typeof 
+    
+    문자열인지 숫자인지 데이터 타입을 확인할 수 있다.
+
+    문자열은 string
+
+    숫자는 number
+*/
+
+typeof 1;
+// 결과값 number
+
+
+typeof "1";
+// 결과값 string
