bea8efd83e78f77824fcdbb40e9413056f665b6a
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-05-12 10:46:00 +0900

    Update class.js

--- js/Class 문법/class.js
+++ js/Class 문법/class.js
@@ -38,3 +38,21 @@ var Yoimiya = new skill2('유금 운간초', '염초 정화의 춤');
 
 var Ayaka = new skill2('멸망의 서리', '얼음꽃');
 // log 결과 - skill {q: '멸망의 서리', e: '얼음꽃'}
+
+
+//prototype은 유전자라고 이해하면 쉽다.
+
+//아래는 부모인 skill 유전자에 name을 추가한 것이다.
+skill.prototype.name = "요이미야";
+
+var Yoimiya = new skill('유금 운간초', '염초 정화의 춤');
+
+Yoimiya.name;
+//log 결과 - '요이미야'
+// skill 함수에 직접 추가하지 않았음에도 name의 값의 출력되는 것을 볼 수 있다.
+
+/* 
+    자바스크립트는 객체에 존재하지 않는 key를 찾을 때 prototype을 탐색하는 과정을 거친다.
+    따라서, skill에 없는 name 객체를 skill의 prototype에 추가된 name 객체에서 찾는 것.
+*/
+
