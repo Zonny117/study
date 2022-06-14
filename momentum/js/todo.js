427dbd84b02d7a1b45983784e5628c1413f28685
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-06-14 10:42:34 +0900

    Update todo.js

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -6,7 +6,15 @@ const toDos = [];
 
 
 function saveTodos() {
-    localStorage.setItem('todos', toDos);
+    /* 
+        localStorage는 무조건 문자열 방식으로만 데이터를 저장한다.
+        따라서 배열의 형태를 유지한채 저장하기 위해선 배열을 문자열 그대로 
+        localStorage로 보내야하는데, 이때 JSON.stringify()를 사용한다.
+
+        JSON.stringify();
+        - 객체 혹은 배열이든 어떠한 형태도 문자열로 변환하는 기능이 있다.
+    */
+    localStorage.setItem('todos', JSON.stringify(toDos));
 }
 
 function deleteTodo(e) {
