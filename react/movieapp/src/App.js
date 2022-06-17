1b32ab0e128293aac458fff5604606aeb16eb884
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-06-17 10:42:21 +0900

    ues effect

--- react/movieapp/src/App.js
+++ react/movieapp/src/App.js
@@ -1,11 +1,24 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+
+  function onClick() {
+    setValue(function (prev) {
+      return prev + 1;
+    });
+  }
+
+  function once() {
+    console.log('once');
+  }
+
+  useEffect(once, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
