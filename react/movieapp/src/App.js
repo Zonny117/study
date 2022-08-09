2fa0675d57e1c71ae0b915cf5e521e632385c67b
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-08-09 17:42:01 +0900

    react-router-dom@6

---
+++ react/movieapp/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movie" element={<Detail></Detail>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
