96ab28f5174be74de33446b30f2b174d4df8c495
Won <87288370+Zonny117@users.noreply.github.com>
2022-09-23 11:45:25 +0900

    1

---
+++ react/nwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAftlZHR53hxehsQQCnxwfOFrmlZWKP5O8",
+    authDomain: "nwitter-5e67a.firebaseapp.com",
+    projectId: "nwitter-5e67a",
+    storageBucket: "nwitter-5e67a.appspot.com",
+    messagingSenderId: "347062751818",
+    appId: "1:347062751818:web:a61bc25c2bb47558514cb3"
+};
+
+export default firebase.initializeApp(firebaseConfig);
