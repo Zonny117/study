eb7ea8fefbfca0f4c99400697d5dcf5c888ab35e
Won <87288370+Zonny117@users.noreply.github.com>
2022-11-21 12:37:18 +0900

    dart

--- flutter/app/bin/app.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  // 변수선언
-  var name = '하이팩토리';
-
-  print(name);
-
-}
---
+++ flutter/app/bin/variable_datatype.dart
@@ -0,0 +1,37 @@
+void main() {
+  // 변수선언
+  var name = '하이팩토리';
+
+  print(name);
+
+  var name2 = '요이미야';
+
+  print(name2);
+
+
+  name = '플러터 프로그래밍';
+
+  print(name);
+
+  //정수타입 integer
+  int number1 = 10;
+
+  print(number1);
+
+  int number2 = 15;
+
+  print(number2);
+  
+  int number3 = -20;
+
+  print(number3);
+
+  // 실수 double
+  double number4 = 2.5;
+  double number5 = 0.5;
+
+  print(number4 + number5);
+
+  // 불리언
+  bool isTrue = true;
+}
