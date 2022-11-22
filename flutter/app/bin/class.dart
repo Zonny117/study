cdec7731ca5663189115b13dc672aa5721a6d3ba
Won <87288370+Zonny117@users.noreply.github.com>
2022-11-22 10:25:20 +0900

    Create class.dart

---
+++ flutter/app/bin/class.dart
@@ -0,0 +1,14 @@
+void main() {}
+
+class Idol {
+  String name = '블랙핑크';
+  List<String> members = ['지수', '제니', '리사', '로제'];
+
+  void sayHello(){
+    print('안녕하세요 블랙핑크입니다.');
+  }
+
+  void introduce(){
+    print('저희 멤버는 지수, 제니, 리사, 로제가 있습니다.');
+  }
+}
