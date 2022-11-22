7c88f8272d575ffccc17ab2c67add98928b445fb
Won <87288370+Zonny117@users.noreply.github.com>
2022-11-22 13:19:48 +0900

    Update class_getter_setter.dart

--- flutter/app/bin/class_getter_setter.dart
+++ flutter/app/bin/class_getter_setter.dart
@@ -12,6 +12,13 @@ void main() {
   //getter
   print(blackPink.firstMember);
   print(youAndI.firstMember);
+  
+
+  //setter - 하나의 패러미터만 받을 수 있기 때문에 = 다음에 오는 것이 setter의 패러미터로 들어온다.
+  blackPink.firstMember = '지드래곤';
+
+  print(blackPink.firstMember);
+  
 }
 
 /* 
@@ -36,8 +43,13 @@ class Idol {
     print('저희 멤버는 ${this.members}가 있습니다.');
   }
 
-  //getter - get 작명{}
+  //getter - get 작명{} - 간단한 데이터 가공을 할때 주로 사용한다.
   String get firstMember {
     return this.members[0];
+  }
+
+  //setter - set 작명(반드시 한개의 패러미터만 올수있다.){} - 잘안쓰는 기능, 왜냐하면 보통 데이터를 임의로 변경하는 것을 막기 위해 final을 선언하면, set은 작동할 수가 없다.
+  set firstMember(String name){
+    this.members[0] = name;
   }
 }
