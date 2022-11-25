b13f3e429d5e3bdeb6d1d82b7ff714b7fe5c2828
Won <87288370+Zonny117@users.noreply.github.com>
2022-11-25 12:51:16 +0900

    Update await.dart

--- flutter/app/bin/async/await.dart
+++ flutter/app/bin/async/await.dart
@@ -1,21 +1,19 @@
-import 'dart:ffi';
-
-import '../basic/function.dart';
-
-void main() {
+void main() async {
   // Future 미래에 받아올 값
   Future<String> name = Future.value('하이팩토리');
   Future<int> number = Future.value(1);
   Future<bool> isTrue = Future.value(true);
 
-  addNumbers(1, 1);
+  final result1 = await addNumbers(1, 1);
   // await을 만났다고 해서 cpu가 작업을 중단하지 않는다는 증거.
   // await을 만나면 마냥 기다리지 않고, 읽어들일만한 코드가 또 있는지 찾게 된다.
-  addNumbers(2, 2);
+  final result2 = await addNumbers(2, 2);
+
+  print('result : $result1, $result2');
 }
 
-// await을 사용하기 위해선 async 키워드가 무조건 선언되어야함
-void addNumbers(int number1, int number2) async {
+// await을 사용하기 위해선 async 키워드가 무조건 선언되어야함, 또한 Future를 리턴해주는 놈만 사용이 가능하다.
+Future<int> addNumbers(int number1, int number2) async {
   print('계산시작 : $number1 + $number2');
 
   // 코드가 실행되다가 await 키워드를 만나면 해당 라인의 코드가 실행이 완료될 때까지 해당 코드 다음에 있는 놈들은 실행안됨.
@@ -25,4 +23,6 @@ void addNumbers(int number1, int number2) async {
   });
 
   print('함수완료');
+
+  return number1 + number2;
 }
