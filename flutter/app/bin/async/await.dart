8d1888fd38daaf377c393fc622233891af0b72c5
Won <87288370+Zonny117@users.noreply.github.com>
2022-11-25 12:36:15 +0900

    async

--- flutter/app/bin/async/await.dart
+++ flutter/app/bin/async/await.dart
@@ -1,5 +1,7 @@
 import 'dart:ffi';
 
+import '../basic/function.dart';
+
 void main() {
   // Future 미래에 받아올 값
   Future<String> name = Future.value('하이팩토리');
@@ -7,13 +9,18 @@ void main() {
   Future<bool> isTrue = Future.value(true);
 
   addNumbers(1, 1);
+  // await을 만났다고 해서 cpu가 작업을 중단하지 않는다는 증거.
+  // await을 만나면 마냥 기다리지 않고, 읽어들일만한 코드가 또 있는지 찾게 된다.
+  addNumbers(2, 2);
 }
 
-void addNumbers(int number1, int number2) {
-  print('계산시작');
+// await을 사용하기 위해선 async 키워드가 무조건 선언되어야함
+void addNumbers(int number1, int number2) async {
+  print('계산시작 : $number1 + $number2');
 
-  // Future.delayed(Duration(지연시간), 지연시간뒤 실행할 함수);
-  Future.delayed(Duration(seconds: 2), () {
+  // 코드가 실행되다가 await 키워드를 만나면 해당 라인의 코드가 실행이 완료될 때까지 해당 코드 다음에 있는 놈들은 실행안됨.
+  // await 키워드 코드가 실행 완료되면, 다시 정상적으로 코드를 읽기 시작함.
+  await Future.delayed(Duration(seconds: 2), () {
     print('계산 완료 : $number1 + $number2 = ${number1 + number2}');
   });
 
--- flutter/app/bin/async/future.dart
+++ flutter/app/bin/async/future.dart
@@ -7,6 +7,10 @@ void main() {
   Future<bool> isTrue = Future.value(true);
 
   addNumbers(1, 1);
+  // 비동기 프로그래밍 - cpu 쓰레드는 작업을 처리하는 가장 작은 단위.
+  // 쓰레드가 작업 하나를 진행하는 동안, 멈추지 않고 다른 작업도 진행하는 것이 방식이다.
+  // 따라서 Future.delayed를 기다리는 동안 아래 함수도 실행되고 있는 것을 확인할 수 있다.
+  addNumbers(2, 2);
 }
 
 void addNumbers(int number1, int number2) {
