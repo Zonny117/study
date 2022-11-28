517ab782543bcad8aca2436a13a12d70848090a2
Won <87288370+Zonny117@users.noreply.github.com>
2022-11-28 12:35:59 +0900

    flutter

--- flutter/contact/lib/main.dart
+++ flutter/contact/lib/main.dart
@@ -10,7 +10,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: Container(
+        // 플러터의 단위는 픽셀이 아닌 LP이다.
+        width: 50,
+        height: 50,
+        color: Colors.blue,
+      ),
     );
   }
 }
+
+/* 
+  이미지 추가법
+  Images.asset('경로') 
+  경로는 pubspec.yaml 파일에 flutter: 밑에 
+  assets:
+    - assets/
+  라고 작성. 앞에 띄어쓰기 중요!! asset이란 폴더 안에 있는 이미지 파일을 전부 불러옴.
+
+  
+ */
