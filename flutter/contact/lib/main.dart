9b1bdb944ccbc7a43b88b75f728f7fd966466787
Won <87288370+Zonny117@users.noreply.github.com>
2022-11-28 14:13:02 +0900

    Update main.dart

--- flutter/contact/lib/main.dart
+++ flutter/contact/lib/main.dart
@@ -14,32 +14,15 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text('앱임'),
           ),
-          body: Container(
-            child: Text('안녕'),
-          ),
-          bottomNavigationBar: BottomAppBar(
-            child: SizedBox(
+          body: Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: double.infinity,
               height: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.phone),
-                  Icon(Icons.message),
-                  Icon(Icons.contact_page),
-                ],
-              ),
+              color: Colors.blue,
             ),
-          ) 
+          )
         )
     );
   }
 }
-
-/* 
-  이미지 추가법
-  Images.asset('경로') 
-  경로는 pubspec.yaml 파일에 flutter: 밑에 
-  assets:
-    - assets/
-  라고 작성. 앞에 띄어쓰기 중요!! asset이란 폴더 안에 있는 이미지 파일을 전부 불러옴.
- */
