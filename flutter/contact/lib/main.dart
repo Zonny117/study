605840bdd3249c0e06837b06e023ec723d2dffc4
Won <87288370+Zonny117@users.noreply.github.com>
2022-11-30 10:25:43 +0900

    Update main.dart

--- flutter/contact/lib/main.dart
+++ flutter/contact/lib/main.dart
@@ -11,42 +11,47 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color(0xffffffff),
-            title: Text( '금호동3가', style: TextStyle(color:Colors.black, fontWeight: FontWeight.w600)),
-            leading: Icon(Icons.arrow_drop_down, color: Colors.black),
-            shadowColor: Color(0xc1c1c1c1),
-            actions: [Icon(Icons.search, color: Colors.black), Icon(Icons.menu, color: Colors.black), Icon(Icons.alarm, color: Colors.black)],
-          ),
-          body: Container(
-            height: 150,
-            padding: EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(child: Image.asset('assets/dog.png'), margin: EdgeInsets.fromLTRB(0, 0, 20, 0)),
-                Expanded(
-                  child:  Column(
-                  mainAxisAlignment:MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('캐논 DSLR 100D(단렌즈, 충전기 16기가SD 포함', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600)),
-                    Text('성동구 행당동 · 끌올 10분전', style: TextStyle(color: Colors.grey),),
-                    Text('210,000원', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4')
-                      ]
-                    )
-                  ],
-                )
-                ),
-              ],
+          appBar: AppBar(),
+          /* 
+            ListView.builder(itemCount, itemBuilder) 
+            - for문을 사용하지 않고 반복적으로 위젯을 생성할 수 있다.
+            - 패러미터는 2개가 필요하며, itemCount는 반복할 횟수, itemBuilder는 실행할 함수를 받는다.
+            - itemBuilder는 context, index 패러미터 2개가 필요하며, index는 반복문이 돌면서 +1씩 증가하는 정수이다.
+            - itemBuilder는 생성할 위젯이 있어야하기 때문에 당연히 리턴값이 필요하다.
+
+            ListTile()
+            - Row로 일일히 생성하다보면 코드가 너무 길어짐, 이걸 축약해서 간단하게 만들 수 있는 위젯이다.
+           */
+          body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) => 
+            ListTile(
+              leading: Icon(Icons.man),
+              title: Text('홍길동'),
             ),
           ),
+          bottomNavigationBar: bottomAppbar(),
         )
+    );
+  }
+}
+
+class bottomAppbar extends StatelessWidget {
+  const bottomAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(Icons.phone),
+          Icon(Icons.message),
+          Icon(Icons.menu),
+        ],
+      ),
     );
   }
 }
