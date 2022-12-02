3e1cc772c61b375bba14152cd280b496e8110a99
Won <87288370+Zonny117@users.noreply.github.com>
2022-12-02 12:39:09 +0900

    Update 01.variable.dart

--- flutter/app/bin/nomad_study/01.variable.dart
+++ flutter/app/bin/nomad_study/01.variable.dart
@@ -6,4 +6,18 @@ void main() {
   // 변수의 타입을 아예 지정할 수 있음. ex) String, int, double, List, Map 등..
   String name2 = '조니'; // String 타입 선언 - name2라는 변수는 String만 받을 수 있음. (대문자 주의)
   name2 = '아이유'; //String만 받음.
+
+  // dynamic - 어떤 타입이든 전부 값으로 넣을 수 있음.
+  var name3; // 아무런 값도 주지 않으면 dynamic 타입으로 지정됨
+  dynamic name4; // 혹은 dynamic 키워드 선언
+
+  name3 = true;
+  name3 = 1;
+  name3 = '조니'; //어떤 타입이든 전부 대체되는 것을 확인할 수 있다.
+
+  /* 
+    null safety = null 값을 제대로 체크해야 런타임 에러를 방지할 수 있다.
+    타입 지정과 null 체크는 앱 처리속도를 좀더 빠르게 만들어주기 떄문에 
+    사용자가 앱을 이용할 때 더욱 쾌적한 환경을 만들 수 있다.
+   */
 }
