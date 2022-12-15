f57390a59fce5b137b9f076323685b4b3225bef0
Won <87288370+Zonny117@users.noreply.github.com>
2022-12-15 13:21:23 +0900

    auth

--- flutter/auth/lib/main.dart
+++ flutter/auth/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'chat_screen.dart';
 
 void main() async {
   // firebase를 사용하려면 반드시 비동기 방식으로 플러터 코어 엔진 초기화
@@ -17,6 +19,40 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Authentication(),
+    );
+  }
+}
+
+class Authentication extends StatelessWidget {
+  const Authentication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (ctx, snap) {
+        if (!snap.hasData) {
+          return SignInScreen(
+            providerConfigs: [
+              EmailProviderConfiguration(),
+            ],
+          );
+        }
+        return ChatScreen();
+      },
+    );
   }
 }
