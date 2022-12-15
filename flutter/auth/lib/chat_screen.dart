d913eef049cc3f4d2158403fda8861d69f83e841
Won <87288370+Zonny117@users.noreply.github.com>
2022-12-15 13:33:03 +0900

    auth

--- flutter/auth/lib/chat_screen.dart
+++ flutter/auth/lib/chat_screen.dart
@@ -1,11 +1,33 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('하이팩토리'),
+      ),
+      body: Container(
+        child: Text('메인페이지'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.usb_rounded), label: 'usb'),
+          BottomNavigationBarItem(icon: Icon(Icons.usb_rounded), label: 'usb')
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseAuth.instance.signOut();
+        },
+        child: Text(
+          '로그아웃',
+          style: TextStyle(fontSize: 12),
+        ),
+      ),
+    );
   }
 }
