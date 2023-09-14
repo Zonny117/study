9db20e20f4e7503f28213b18cccdd79e397a71aa
zonny117 <wonj1208@naver.com>
2023-09-14 17:57:53 +0900

    reupdate

--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
-import { createStore } from './../node_modules/redux/src/createStore';
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const ADD = 'ADD';
-const MINUS = 'MINUS';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-number.innerText = 0;
-
-const countModifire = (state = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return state + 1;
-    case MINUS:
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
-const countStore = createStore(countModifire);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
-};
-
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
