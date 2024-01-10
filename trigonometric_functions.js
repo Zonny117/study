6109e57b0b7c5cf2d97d9b27337610ec131d36b6
zonny117 <wonj1208@naver.com>
2024-01-10 12:00:58 +0900

    update

--- trigonometric_functions.js
+++ trigonometric_functions.js
@@ -1,6 +1,5 @@
 const circle = document.getElementById('circle');
 const size = circle.getBoundingClientRect().width / 2;
-const standard = size - 15; //#circle의 정가운데이면서 .dot 크기의 절반
 const length = 360 / 20; //원에 20도씩 .dot을 배치
 
 /* 
@@ -16,10 +15,10 @@ const length = 360 / 20; //원에 20도씩 .dot을 배치
     수학함수 cos, sin, tan는 전부 이 라디안 단위를 인자로 받는다. 따라서 특정 각도를 라디안 단위로 변환해서 
     해당 함수들로 활용해야한다.
 
-    [각도 변환]
+    [라디안 변환]
     degree * (PI/180) = radian
 
-    [라디안 변환]
+    [각도 변환]
     radian * (180/PI) = degree
 
     [삼각함수]
@@ -30,13 +29,22 @@ const length = 360 / 20; //원에 20도씩 .dot을 배치
 
 for (let i = 0; i < length; i++) {
   const rad = i * 20 * (Math.PI / 180); //라디안으로 변환
-  const x = standard + size * Math.cos(rad); // x좌표
-  const y = standard + size * Math.sin(rad); // y좌표
+  const x = size * Math.cos(rad); // x좌표
+  const y = size * Math.sin(rad); // y좌표
 
   const dot = document.createElement('span');
   dot.className = 'dot';
-  dot.style.top = `${y}px`;
-  dot.style.left = `${x}px`;
+  dot.style.transform = `translate(${x}px, ${y}px)`;
 
   circle.appendChild(dot);
 }
+
+const eyeball = document.getElementById('eyeball');
+
+document.addEventListener('mousemove', function (e) {
+  let x = (e.pageX * 100) / window.innerWidth + '%';
+  let y = (e.pageY * 100) / window.innerHeight + '%';
+
+  eyeball.style.left = x;
+  eyeball.style.top = y;
+});
