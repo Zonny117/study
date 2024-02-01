aa25b03b2cd812217e9db49d4c6daef2460402e5
zonny117 <wonj1208@naver.com>
2024-02-01 11:55:57 +0900

    form

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,10 @@
 <template>
-  <div>
-    <!-- 
-    <h1>{{ count }}</h1>
-    <button v-on:click="addNumber(1)">증가</button>
-    <button v-on:click="minusNumber">감소</button>
-    <button v-on:mouseover="addNumber(10)">마우스 오버 10 증가</button> 
-    -->
-    <h2>{{ message }}</h2>
-    <!-- 
-    <button @click="greeting">인사하기</button>
-    <button @click="sayGoodbye">작별하기</button>
-    <div class="box" @click="getMousePosition"></div> 
-    -->
-    <!-- 
-      이벤트 객체도 동시에 받기 위해서는 $event라고 패러미터에 명시해 주어야한다.
-     -->
-    <button @click="addNumber($event, 10)">10더하기</button>
-    <button @click="addNumber($event, 100)">100더하기</button>
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  data() {
-    return {
-      count: 0,
-      message: '',
-    };
-  },
-  methods: {
-    greeting() {
-      this.message = '안녕하세요';
-    },
-    sayGoodbye() {
-      this.message = '바이';
-    },
-    addNumber(e, value) {
-      this.message = `마우스 좌표 ${e.pageX}와 더하기 ${value}는 ${
-        e.pageX + value
-      }입니다.`;
-    },
-    minusNumber() {
-      this.count = this.count - 1;
-    },
-    getMousePosition(e) {
-      this.message = `마우스의 위치는 ${e.pageX}, ${e.pageY}`;
-    },
-  },
 };
 </script>
 
@@ -59,18 +16,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-button {
-  display: inline-block;
-  vertical-align: middle;
-  width: 100px;
-  height: 100px;
-  font-size: 24px;
-}
-
-.box {
-  width: 300px;
-  height: 300px;
-  background: salmon;
 }
 </style>
---
+++ src/form.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    {{ user }}
+    <form>
+      <div>
+        <label for="name">이름</label>
+        <input type="text" id="name" v-model="user.name" @input="setValue" />
+      </div>
+      <div>
+        <label for="age">나이</label>
+        <input type="text" id="age" v-model="user.age" />
+      </div>
+      <div>
+        <label for="city">사는 곳</label>
+        <!--
+          data에 city 값을 따로 선언하지 않아도
+          v-model을 통해 생성할 수 있다.
+          하지만 가급적 선언해주는 것이 좋으며,
+          어떤 타입의 데이터인지도 명시하는게 좋다(ts).
+         -->
+        <select id="city" v-model="user.city">
+          <option value="seoul">서울</option>
+          <option value="daejeon">대전</option>
+          <option value="daegu">대구</option>
+          <option value="busan">부산</option>
+          <option value="gwangju">광주</option>
+        </select>
+      </div>
+      <div>
+        <label for="favorite-food">좋아하는 음식</label>
+        <select id="favorite-food" multiple v-model="user.favorite">
+          <option
+            :key="food.code"
+            v-for="food in foodOptions"
+            :value="food.code"
+          >
+            {{ food.label }}
+          </option>
+        </select>
+      </div>
+      <div>
+        <label for="job">직업</label>
+        프로그래머
+        <input type="checkbox" value="programmer" v-model="user.job" /> 가수
+        <input type="checkbox" value="singer" v-model="user.job" /> 교사
+        <input type="checkbox" value="teacher" v-model="user.job" />
+      </div>
+      <div>
+        <label for="sex">성별</label>
+        <input type="radio" value="male" v-model="user.gender" />
+        <input type="radio" value="female" v-model="user.gender" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      foodOptions: [
+        { label: '짜장면', code: 'JJ' },
+        { label: '짬뽕', code: 'JB' },
+        { label: '탕수육', code: 'TS' },
+      ],
+      user: {
+        name: '',
+        age: 0,
+        city: 'seoul',
+        favorite: [],
+        job: [],
+        gender: '',
+      },
+    };
+  },
+  methods: {
+    setValue(e) {
+      const { value } = e.target;
+      this.user.name = value;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+label {
+  margin-right: 1rem;
+  vertical-align: middle;
+  font-size: 22px;
+  font-weight: bold;
+}
+div {
+  margin-bottom: 1rem;
+}
+</style>
