d091fa6f351ac0bb43379b0fd9474985fbf84abf
zonny117 <wonj1208@naver.com>
2025-01-15 11:17:31 +0900

    vue3 테일윈드

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavgitaion />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import SiteNavgitaion from "./components/SiteNavgitaion.vue";
+</script>
+<style lang="scss"></style>
