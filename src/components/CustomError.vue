232ee00948b951fec0f527db5e22e058b0de49f4
zonny117 <wonj1208@naver.com>
2025-01-20 15:54:18 +0900

    vee validate type

---
+++ src/components/CustomError.vue
@@ -0,0 +1,15 @@
+<template>
+  <ErrorMessage :name="props.name" class="mt-2 block font-bold text-red-600" />
+</template>
+
+<script setup lang="ts">
+import { ErrorMessage } from 'vee-validate';
+
+interface Props {
+  name: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped></style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -12,8 +12,10 @@
         모바일 보안카드 발급 시스템
       </h1>
       <p class="mb-[54px] text-lg">안내문구가 들어가는 영역입니다.</p>
-      <Form class="w-[469px]">
+
+      <form @submit="onSubmit" class="w-[469px]">
         <InputBox field-name="userName" place-holder="ID" />
+        <CustomError name="userName" />
         <InputBox
           field-name="password"
           place-holder="비밀번호"
@@ -21,21 +23,56 @@
           :input-type="isVisible ? 'text' : 'password'"
           @toggle-password="togglePassword"
         />
-      </Form>
+        <CustomError name="password" />
+        <div class="group mt-3 flex cursor-pointer items-center gap-2">
+          <Field
+            id="autoLogin"
+            name="autoLogin"
+            type="checkbox"
+            class="group-hover:cursor-pointer"
+            :value="true"
+            :unchecked-value="false"
+          />
+          <label
+            for="autoLogin"
+            class="text-[#585858] group-hover:cursor-pointer"
+            >아이디 저장</label
+          >
+        </div>
+        <button type="button" class="btn-secondary mt-8">OTP 발송</button>
+        <button type="submit" class="btn-primary mt-3.5">로그인</button>
+      </form>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import CustomError from '@/components/CustomError.vue';
 import InputBox from '@/components/InputBox.vue';
-import { Form } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/yup';
+import { ErrorMessage, Field, useForm } from 'vee-validate';
 import { ref } from 'vue';
+import * as yup from 'yup';
+
+const { handleSubmit } = useForm({
+  validationSchema: toTypedSchema(
+    yup.object().shape({
+      userName: yup.string().required('아이디를 입력하세요.'),
+      password: yup.string().required('비밀번호를 입력하세요.'),
+      autoLogin: yup.boolean(),
+    })
+  ),
+});
 
 const isVisible = ref(false);
 
 const togglePassword = () => {
   isVisible.value = !isVisible.value;
 };
+
+const onSubmit = handleSubmit(values => {
+  console.log(values);
+});
 </script>
 
 <style scoped></style>
